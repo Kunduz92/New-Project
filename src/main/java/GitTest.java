aa845f338ec5ab21e959a71e9474fb07dade3978
Kunduz <kunduz.sydykova@gmail.com>
2021-03-17 12:13:50 -0400

    new addition

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,6 +3,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("100_000.00$");
         System.out.println("ScrumRunner Branch");
+        System.out.println("Coding");
+        System.out.println("Testing");
 
     }
 
